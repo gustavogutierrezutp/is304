59c1eeae0c98f22633bd2be019e54180ab57e19b
Gustavo Gutiérrez-Sabogal <gustavo.gutierrez@utp.edu.co>
2025-08-27 09:55:28 -0500

    Implement Vector class with dynamic resizing and element management

---
+++ vector.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T> class Vector {
+private:
+  // Stores the elements of the vector
+  T *storage;
+  // Current number of elements in the vector
+  unsigned int sz;
+  // Maximum number of elements that storage can hold
+  unsigned int capacity;
+  // Policy for resizing the vector
+  double policy;
+
+public:
+  Vector() {
+    storage = new T[5];
+    sz = 0;
+    capacity = 5;
+    policy = 1.5;
+  }
+
+  Vector(unsigned int c, double p = 1.5) {
+    storage = new T[c];
+    sz = 0;
+    capacity = c;
+    policy = p;
+  }
+
+  Vector(const Vector<T> &other) {
+    sz = other.size();
+    capacity = other.capacity;
+    policy = other.policy;
+    storage = new T[capacity];
+    for (unsigned int i = 0; i < sz; i++) {
+      storage[i] = other.storage[i];
+    }
+  }
+
+  ~Vector() { delete[] storage; }
+
+  void push_back(const T &elem) {
+    if (sz == capacity) {
+      resize();
+    }
+    storage[sz] = elem;
+    sz++;
+  }
+
+  void push_back(const Vector<T> &other) {
+    reserve(sz + other.size());
+    for (unsigned int i = 0; i < other.size(); i++) {
+      push_back(other.storage[i]);
+    }
+  }
+
+  void pop_back() { sz--; }
+
+  unsigned int size() const { return sz; }
+
+private:
+  void resize() {
+    capacity *= policy;
+    T *new_storage = new T[capacity];
+    for (unsigned int i = 0; i < sz; i++) {
+      new_storage[i] = storage[i];
+    }
+    delete[] storage;
+    storage = new_storage;
+  }
+
+  void reserve(unsigned int new_capacity) {
+    if (new_capacity > capacity) {
+      capacity = new_capacity;
+      T *new_storage = new T[capacity];
+      for (unsigned int i = 0; i < sz; i++) {
+        new_storage[i] = storage[i];
+      }
+      delete[] storage;
+      storage = new_storage;
+    }
+  }
+
+public:
+  void shrink_to_fit() {
+    if (sz < capacity) {
+      T *new_storage = new T[sz];
+      for (unsigned int i = 0; i < sz; i++) {
+        new_storage[i] = storage[i];
+      }
+      delete[] storage;
+      storage = new_storage;
+      capacity = sz;
+    }
+  }
+
+  T &operator[](unsigned int index) { return storage[index]; }
+
+  const T &operator[](unsigned int index) const { return storage[index]; }
+
+  T &at(unsigned int index) {
+    if (index >= sz) {
+      throw out_of_range("Index out of range");
+    }
+    return storage[index];
+  }
+
+  const T &at(unsigned int index) const {
+    if (index >= sz) {
+      throw out_of_range("Index out of range");
+    }
+    return storage[index];
+  }
+};
+
+void printVector(const Vector<int> &v) {
+  for (unsigned int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+
+class Point {
+private:
+  double x, y;
+
+public:
+  Point(double x = 0, double y = 0) : x(x), y(y) {}
+};
+
+int main() {
+  Vector<int> u;
+
+  for (int i = 0; i < 10; i++) {
+    u.push_back(i);
+  }
+
+  printVector(u);
+
+  u[3] = 100;
+
+  printVector(u);
+  return 0;
+}
