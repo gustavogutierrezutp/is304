dda6d6499ae1c6bddd111ea4a268ffd1d8d9303e
davidgrmn <david.grajales1@utp.edu.co>
2025-09-01 20:17:22 -0500

    Update vector.cpp
    Original vector class + 5 exercises

--- vector.cpp
+++ vector.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-
 using namespace std;
+
 
 template <typename T> class Vector {
 private:
@@ -21,7 +21,8 @@ public:
     policy = 1.5;
   }
 
-  Vector(unsigned int c, double p = 1.5) {
+  Vector(unsigned int c, double p = 1.5) 
+  {
     storage = new T[c];
     sz = 0;
     capacity = c;
@@ -40,41 +41,60 @@ public:
 
   ~Vector() { delete[] storage; }
 
-  void push_back(const T &elem) {
-    if (sz == capacity) {
+  void push_back(const T &elem) 
+  {
+    if (sz == capacity) 
+    {
       resize();
     }
     storage[sz] = elem;
     sz++;
   }
 
-  void push_back(const Vector<T> &other) {
-    reserve(sz + other.size());
-    for (unsigned int i = 0; i < other.size(); i++) {
-      push_back(other.storage[i]);
-    }
+  
+  
+  
+
+  void pop_back() 
+  { 
+    sz--; 
   }
 
-  void pop_back() { sz--; }
+  unsigned int size() const 
+  { 
+    return sz; 
+  }
 
-  unsigned int size() const { return sz; }
+  unsigned int get_capacity() const 
+  { 
+    return capacity; 
+  }
+
+
+
 
 private:
-  void resize() {
+
+  void resize() 
+  {
     capacity *= policy;
     T *new_storage = new T[capacity];
-    for (unsigned int i = 0; i < sz; i++) {
+    for (unsigned int i = 0; i < sz; i++) 
+    {
       new_storage[i] = storage[i];
     }
     delete[] storage;
     storage = new_storage;
   }
 
-  void reserve(unsigned int new_capacity) {
-    if (new_capacity > capacity) {
+  void reserve(unsigned int new_capacity) 
+  {
+    if (new_capacity > capacity) 
+    {
       capacity = new_capacity;
       T *new_storage = new T[capacity];
-      for (unsigned int i = 0; i < sz; i++) {
+      for (unsigned int i = 0; i < sz; i++) 
+      {
         new_storage[i] = storage[i];
       }
       delete[] storage;
@@ -83,10 +103,13 @@ private:
   }
 
 public:
-  void shrink_to_fit() {
-    if (sz < capacity) {
+  void shrink_to_fit() 
+  {
+    if (sz < capacity) 
+    {
       T *new_storage = new T[sz];
-      for (unsigned int i = 0; i < sz; i++) {
+      for (unsigned int i = 0; i < sz; i++) 
+      {
         new_storage[i] = storage[i];
       }
       delete[] storage;
@@ -95,51 +118,154 @@ public:
     }
   }
 
-  T &operator[](unsigned int index) { return storage[index]; }
+  T &operator[](unsigned int index) 
+  { 
+    return storage[index]; 
+  }
 
-  const T &operator[](unsigned int index) const { return storage[index]; }
+  const T &operator[](unsigned int index) const 
+  { 
+    return storage[index]; 
+  }
 
-  T &at(unsigned int index) {
-    if (index >= sz) {
+  T &at(unsigned int index) 
+  {
+    if (index >= sz) 
+    {
       throw out_of_range("Index out of range");
     }
     return storage[index];
   }
 
-  const T &at(unsigned int index) const {
-    if (index >= sz) {
+  const T &at(unsigned int index) const 
+  {
+    if (index >= sz) 
+    {
       throw out_of_range("Index out of range");
     }
     return storage[index];
   }
+
 };
 
-void printVector(const Vector<int> &v) {
-  for (unsigned int i = 0; i < v.size(); i++) {
-    cout << v[i] << " ";
+
+
+//Exercise #1
+int sumVector(const Vector<int>& v)
+  {
+    int sum = 0;
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        sum+=v[i];
+    }
+
+    return sum;
   }
-  cout << endl;
+
+
+//Exercise #2 
+Vector<int> reverseVector(const Vector<int>& v)
+{
+    Vector<int> reversed(v.size()); 
+    
+    for (unsigned int i=0; i<v.size(); i++)
+    {
+        reversed.push_back(v[v.size() - i - 1]);
+    }
+
+    return reversed;
+    
 }
 
-class Point {
-private:
-  double x, y;
 
-public:
-  Point(double x = 0, double y = 0) : x(x), y(y) {}
-};
+//Exercise #3
+Vector<int> filterEven(const Vector<int>& v)
+{
+    Vector<int> evens;
 
-int main() {
-  Vector<int> u;
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        if (v[i] % 2 == 0)
+        {
+            evens.push_back(v[i]);
+        }
+    }
 
-  for (int i = 0; i < 10; i++) {
-    u.push_back(i);
-  }
+    return evens;
+}
 
-  printVector(u);
 
-  u[3] = 100;
+//Exercise #4
+void Dynamic_Growth_Test()
+{
+    Vector<int> test_v;
+    unsigned int current_capacity = test_v.get_capacity();
+    
+    for (unsigned int i=0; i<1000; i++)
+    {
+        test_v.push_back(i);
 
-  printVector(u);
+        if (test_v.get_capacity() != current_capacity)
+        {
+            cout << "Capacity: " << test_v.get_capacity() << endl;
+            cout << "Size: " << test_v.size() << endl;
+            current_capacity = test_v.get_capacity();
+            cout << endl;
+        }
+
+
+    }
+}
+
+//Exercise #5
+Vector<int> mergeSorted(const Vector<int>& a, const Vector<int>& b)
+{
+    Vector<int> merged(a.size() + b.size());
+    unsigned int i = 0;
+    unsigned int j = 0;
+
+    while (i<a.size() and j<b.size())
+    {
+
+        if (a[i] < b[j])
+        {
+            merged.push_back(a[i]);
+            i++;
+        }
+            
+        else
+        {
+            merged.push_back(b[j]);
+            j++;
+        }
+        
+    }
+
+
+    while(j<b.size())
+        {
+            merged.push_back(b[j]);
+            j++;
+        }
+    
+
+  
+    while(i<a.size())
+        {
+            merged.push_back(a[i]);
+            i++;
+        }
+       
+
+    
+    return merged;
+}
+
+
+
+
+
+int main() 
+{
   return 0;
 }
